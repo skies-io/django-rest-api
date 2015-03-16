599bc39c3f941228441367341dd7bec41bf254d4
Maigret Aurélien <dewep.net@gmail.com>
2015-03-16 17:23:28 +0100

    Useless model

--- example/example/models.py
+++ example/example/models.py
@@ -1,15 +1,14 @@
 # -*- coding: utf-8 -*-
 
 from django.db import models
-from example.model import BaseExampleModel
 
 
-class User(BaseExampleModel):
+class User(models.Model):
     email = models.EmailField(max_length=64, unique=True)
     password = models.CharField(max_length=128)
 
 
-class Project(BaseExampleModel):
+class Project(models.Model):
     owner = models.ForeignKey(User)
     contributors = models.ManyToManyField(User, related_name='project_contributors')
     name = models.CharField(max_length=128)
